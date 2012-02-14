faed2ad6c22524d1c14407ec48a91ae66faeb71e
Delisa Mason <iskanamagus@gmail.com>
2012-02-14 14:41:38 -0500

    Set up sinatra config, update readme

--- app.rb
+++ app.rb
@@ -3,32 +3,58 @@ require 'sinatra'
 require 'haml'
 require 'pivotal-tracker'
 
-def parse_stories api_token, name
-  PivotalTracker::Client.token = api_token
+def get_stories
+  PivotalTracker::Client.token = user[:token]
   projects = PivotalTracker::Project.all
   stories = projects.map {|project|
-    project.stories.all(:owner => name)
-  }.flatten
+    project.stories.all(:owner => user[:name])
+  }.flatten.sort_by {|s| s.name}.sort_by {|s|s.current_state}
 end
 
-get '/' do
+def authenticate token, name
+  session[:name], session[:token] = name, token
+end
+
+def user
+  {:token => session[:token],:name => session[:name]}
+end
+
+configure do
+  enable :sessions
+  set :app_file, __FILE__
+  set :root, File.dirname(__FILE__)
+  set(:auth) do |*roles|
+    condition do
+      unless logged_in?
+        redirect "/login"
+      end
+    end
+  end
+end
+
+def logged_in?
+  not session[:token].nil?
+end
+
+get '/', :auth => :user do
+  redirect to('/stories')
+end
+
+get '/login' do
   haml :login
 end
 
-post '/stories' do
-  if not session[:token]
-    session[:token] = params[:token]
-    @name = params[:name]
-  end
-  if token = session[:token]
-    @stories = parse_stories(token, @name)
-    haml :stories
-  else
-    haml :login
-  end
+post '/login' do
+  authenticate(params[:token], params[:name])
+  redirect to('/stories')
+end
+
+get '/stories', :auth => :user do
+  @stories = get_stories
+  haml :stories
 end
 
 get '/logout' do
   session[:token] = nil
-  haml :login
+  redirect to('/login')
 end
--- readme.md
+++ readme.md
@@ -6,9 +6,15 @@ keep track of all your tracker stories (across projects) while maintaining your 
 
     ruby app.rb
 
+## tools
+
+- [haml](haml-lang.com)
+- [pivotal-tracker](https://github.com/jsmestad/pivotal-tracker)
+- [sinatra](www.sinatrarb.com)
+- [twitter bootstrap](http://twitter.github.com/bootstrap/)
+
 ## todo
 
 - make it do stuff
 - make it pretty
 - package as a gem
-
